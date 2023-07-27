6499abdb4bb9d0f572c87af8ef3ef35d90fa9f39
Hugo Hakim Damer <hdamer@uni-bremen.de>
2023-07-28 00:02:06 +0200

    Feat add basic support for IPv6 networks
    Newly generated networks automatically generate an IPv6 prefix of size
    64 within the ULA address range, devices obtain a randomly generated
    address within this prefix.
    
    Currently, this is Linux only and does not yet support all features
    (routes currently cause an error).

--- management/server/peer.go
+++ management/server/peer.go
@@ -79,6 +79,8 @@ type Peer struct {
 	SetupKey string
 	// IP address of the Peer
 	IP net.IP
+	// IPv6 address of the Peer
+	IP6 *net.IP
 	// Meta is a Peer system meta data
 	Meta PeerSystemMeta
 	// Name is peer's name (machine name)
@@ -113,6 +115,7 @@ func (p *Peer) Copy() *Peer {
 		Key:                    p.Key,
 		SetupKey:               p.SetupKey,
 		IP:                     p.IP,
+		IP6:                    p.IP6,
 		Meta:                   p.Meta,
 		Name:                   p.Name,
 		Status:                 p.Status,
@@ -543,12 +546,21 @@ func (am *DefaultAccountManager) AddPeer(setupKey, userID string, peer *Peer) (*
 	if err != nil {
 		return nil, nil, err
 	}
+	var nextIp6 *net.IP = nil
+	if network.Net6 != nil {
+		nextIp6tmp, err := AllocatePeerIP6(*network.Net6, takenIps)
+		if err != nil {
+			return nil, nil, err
+		}
+		nextIp6 = &nextIp6tmp
+	}
 
 	newPeer := &Peer{
 		ID:                     xid.New().String(),
 		Key:                    peer.Key,
 		SetupKey:               upperKey,
 		IP:                     nextIp,
+		IP6:                    nextIp6,
 		Meta:                   peer.Meta,
 		Name:                   peer.Meta.Hostname,
 		DNSLabel:               newLabel,
